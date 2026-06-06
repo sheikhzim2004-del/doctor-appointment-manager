b382bc27bb97c875269413ef873812a5a0b75a6d
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-06-07 01:15:23 +0600

    display all doctors card

--- src/app/doctors/page.jsx
+++ src/app/doctors/page.jsx
@@ -1,22 +1,31 @@
+import DoctorCard from '@/components/shared/ui/DoctorCard';
 import React from 'react';
 
 const DoctorsPage = async () => {
 
-  const res = await fetch("http://localhost:8080/doctors")
+  const res = await fetch("http://localhost:8080/doctors", {
+    cache: "no-store"
+  })
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch doctors");
+  }
+
   const data = await res.json();
   console.log(data);
 
 
+
+
   return (
-    <div>
-      {
-        data.map(doctor => (
-          <div key={doctor._id} className='border p-4 rounded-md mb-4'>
-            <h2 className='text-xl font-bold mb-2'>{doctor.name}</h2>
-            <p><strong>Specialty:</strong> {doctor.specialty}</p>
-          </div>
-        ))
-      }
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {
+          data.map(doctor => (
+            <DoctorCard key={doctor._id} doctor={doctor}></DoctorCard>
+          ))
+        }
+      </div>
     </div>
   );
 };
